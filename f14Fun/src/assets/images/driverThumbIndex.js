821d3a06c4b50e222e75312c93fa6687b07f7b6a
Jefferson Capovilla <jeffecap@motorola.com>
2022-03-26 22:51:20 -0300

    Load driver image based on driverID
    Loading image based on driverID. The images are stored
    locally for easier reference based on driverID value obtained
    from JSON data.

---
+++ f14Fun/src/assets/images/driverThumbIndex.js
@@ -0,0 +1,48 @@
+const senninha= { name: "senninha" , image: require('../../assets/images/driverThumb/senninha.jpg')}
+const albon= { name: "albon", image: require('../../assets/images/driverThumb/albon.png') }
+const alonso= { name: "alonso", image: require('../../assets/images/driverThumb/alonso.png') }
+const bottas= { name: "bottas", image: require('../../assets/images/driverThumb/bottas.png') }
+const gasly= { name: "gasly" , image: require('../../assets/images/driverThumb/gasly.png')}
+const hamilton= { name: "hamilton" , image: require('../../assets/images/driverThumb/hamilton.png')}
+const hulkenberg= { name: "hulkenberg" , image: require('../../assets/images/driverThumb/hulkenberg.png')}
+const kevin_magnussen= { name: "kevin_magnussen" , image: require('../../assets/images/driverThumb/kevin_magnussen.png')}
+const latifi= { name: "latifi" , image: require('../../assets/images/driverThumb/latifi.png')}
+const leclerc= { name: "leclerc" , image: require('../../assets/images/driverThumb/leclerc.png')}
+const max_verstappen= { name: "max_verstappen" , image: require('../../assets/images/driverThumb/max_verstappen.png')}
+const mick_schumacher= { name: "mick_schumacher" , image: require('../../assets/images/driverThumb/mick_schumacher.png')}
+const norris= { name: "norris" , image: require('../../assets/images/driverThumb/norris.png')}
+const ocon= { name: "ocon" , image: require('../../assets/images/driverThumb/ocon.png')}
+const perez= { name: "perez" , image: require('../../assets/images/driverThumb/perez.png')}
+const ricciardo= { name: "ricciardo", image: require('../../assets/images/driverThumb/ricciardo.png')}
+const russell= { name: "russell", image: require('../../assets/images/driverThumb/russell.png') }
+const sainz= { name: "sainz" , image: require('../../assets/images/driverThumb/sainz.png')}
+const stroll= { name: "stroll" , image: require('../../assets/images/driverThumb/stroll.png')}
+const tsunoda= { name: "tsunoda" , image: require('../../assets/images/driverThumb/tsunoda.png')}
+const vettel= { name: "vettel" , image: require('../../assets/images/driverThumb/vettel.png')}
+const zhou= { name: "zhou" , image: require('../../assets/images/driverThumb/zhou.png')}
+
+const driverThumb =  [
+        senninha,
+        albon,
+        alonso,
+        bottas,
+        gasly,
+        hamilton,
+        hulkenberg,
+        kevin_magnussen,
+        latifi,
+        leclerc,
+        max_verstappen,
+        mick_schumacher,
+        norris,
+        ocon,
+        perez,
+        ricciardo,
+        russell,
+        sainz,
+        stroll,
+        tsunoda,
+        vettel,
+        zhou,
+    ]
+export default driverThumb;
--- f14Fun/src/screens/DriverDetails.js
+++ f14Fun/src/screens/DriverDetails.js
@@ -1,11 +1,48 @@
 import * as React from 'react';
-import { Dimensions, ImageBackground, Linking, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Image, ImageBackground, Linking, StyleSheet, Text, View } from 'react-native';
+import driverThumbIndex from '../assets/images/driverThumbIndex';
 
 const image = { uri: "https://www.sportsnet.ca/wp-content/uploads/2022/03/F1-1040x572.jpg" };
 const screenSizeHeight = Dimensions.get('window').height
 const screenSizeWidth = Dimensions.get('window').width
 
 export default class DriverDetails extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            driverThumbImage: '',
+        }
+    }
+
+    componentDidMount = () => {
+        this.backgroundChange();
+        // this.printLogData();
+    };
+
+    getDriverSmallById = (driverId) => {
+        // console.log(driverThumbIndex)
+        var driver = driverThumbIndex.find(x => x.name == driverId)
+        if (driver) {
+            // console.log("Driver image selected:", driver.image)
+            return driver.image
+        }
+        // Default driver image
+        return driverThumbIndex[0].image
+    }
+
+    backgroundChange = () => {
+        // TODO: Need to fix this warning on first launch: ERROR  Warning: Failed prop type: Invalid prop `source` supplied to `Image`, expected one of type [number].
+        // Image@http://10.0.2.2:8081/index.bundle?platform=android&dev=true&minify=false&app=com.f14fun&modulesOnly=false&runModule=true:74305:56
+        const driverId = this.props.route.params.item.driverId
+        this.setState({
+            driverThumbImage: this.getDriverSmallById(driverId),
+        })
+        console.log("Driver: ", driverId)
+        console.log("driverThumbImage: ", this.state.driverThumbImage)
+    }
+
+
     render() {
         const { item } = this.props.route.params;
         return (
@@ -21,6 +58,9 @@ export default class DriverDetails extends React.Component {
                     <Text style={styles.bodyStyle}>{`Date of Birth: ${item.dateOfBirth}`}</Text>
                     <Text style={styles.bodyStyle}>{`Nationality: ${item.nationality}`}</Text>
                     <Text style={styles.urlStyle} onPress={() => Linking.openURL(item.url)}>{`Books: ${item.url}`}</Text>
+                </View>
+                <View>
+                    <Image source={this.state.driverThumbImage} />
                 </View>
             </ImageBackground>
         )
