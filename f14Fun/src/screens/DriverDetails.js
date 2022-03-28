940007b2720fa15117d5a09a81f6fb4d43f0a4bf
Jefferson Capovilla <jefcap@gmail.com>
2022-03-28 19:30:09 -0300

    Fix: Load driver image using imageMap
    Previous form of image index was returning the error below during
    the first load.
    ERROR  Warning: Failed prop type: Invalid prop `source` supplied to `Image`, expected one of type [number].
    Image@http://10.0.2.2:8081/index.bundle?platform=android&dev=true&minify=false&app=com.f14fun&modulesOnly=false&runModule=true:74305:56
        in DriverDetails (at SceneView.tsx:126)
        in StaticContainer
        in EnsureSingleNavigator (at SceneView.tsx:118)
        in SceneView (at useDescriptors.tsx:210)
        in RCTView (at View.js:32)
        in View (at DebugContainer.native.tsx:27)
        in DebugContainer (at NativeStackView.native.tsx:76)
        in MaybeNestedStack (at NativeStackView.native.tsx:247)
        in RNSScreen (at createAnimatedComponent.js:211)
        in AnimatedComponent (at createAnimatedComponent.js:264)
        in AnimatedComponentWrapper (at src/index.native.tsx:281)
        in MaybeFreeze (at src/index.native.tsx:280)
        in Screen (at NativeStackView.native.tsx:180)
        in SceneView (at NativeStackView.native.tsx:299)
        in RNSScreenStack (at src/index.native.tsx:202)
        in ScreenStack (at NativeStackView.native.tsx:290)
        in NativeStackViewInner (at NativeStackView.native.tsx:344)
        in RNCSafeAreaProvider (at SafeAreaContext.tsx:87)
        in SafeAreaProvider (at SafeAreaProviderCompat.tsx:46)
        in SafeAreaProviderCompat (at NativeStackView.native.tsx:343)
        in NativeStackView (at createNativeStackNavigator.tsx:69)
        in Unknown (at createNativeStackNavigator.tsx:68)
        in NativeStackNavigator (at MyStack.js:27)
        in EnsureSingleNavigator (at BaseNavigationContainer.tsx:430)
        in BaseNavigationContainer (at NavigationContainer.tsx:132)
        in ThemeProvider (at NavigationContainer.tsx:131)
        in NavigationContainerInner (at MyStack.js:26)
        in MyStack (at App.js:6)
        in App (at renderApplication.js:50)
        in RCTView (at View.js:32)
        in View (at AppContainer.js:92)
        in RCTView (at View.js:32)
        in View (at AppContainer.js:119)
        in AppContainer (at renderApplication.js:43)
        in f14Fun(RootComponent) (at renderApplication.js:60)

--- f14Fun/src/screens/DriverDetails.js
+++ f14Fun/src/screens/DriverDetails.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Dimensions, Image, ImageBackground, Linking, StyleSheet, Text, View } from 'react-native';
 import driverThumbIndex from '../assets/images/driverThumbIndex';
 
-const image = { uri: "https://www.sportsnet.ca/wp-content/uploads/2022/03/F1-1040x572.jpg" };
+const backgroundUri = { uri: "https://www.sportsnet.ca/wp-content/uploads/2022/03/F1-1040x572.jpg" };
 const screenSizeHeight = Dimensions.get('window').height
 const screenSizeWidth = Dimensions.get('window').width
 
@@ -10,44 +10,14 @@ export default class DriverDetails extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            driverThumbImage: '',
-        }
     }
-
-    componentDidMount = () => {
-        this.backgroundChange();
-        // this.printLogData();
-    };
-
-    getDriverSmallById = (driverId) => {
-        // console.log(driverThumbIndex)
-        var driver = driverThumbIndex.find(x => x.name == driverId)
-        if (driver) {
-            // console.log("Driver image selected:", driver.image)
-            return driver.image
-        }
-        // Default driver image
-        return driverThumbIndex[0].image
-    }
-
-    backgroundChange = () => {
-        // TODO: Need to fix this warning on first launch: ERROR  Warning: Failed prop type: Invalid prop `source` supplied to `Image`, expected one of type [number].
-        // Image@http://10.0.2.2:8081/index.bundle?platform=android&dev=true&minify=false&app=com.f14fun&modulesOnly=false&runModule=true:74305:56
-        const driverId = this.props.route.params.item.driverId
-        this.setState({
-            driverThumbImage: this.getDriverSmallById(driverId),
-        })
-        console.log("Driver: ", driverId)
-        console.log("driverThumbImage: ", this.state.driverThumbImage)
-    }
-
 
     render() {
         const { item } = this.props.route.params;
+
         return (
             <ImageBackground
-                source={image}
+                source={backgroundUri}
                 resizeMode="cover"
                 style={styles.imageBackground}>
                 <View style={styles.mainTheme}>
@@ -60,7 +30,7 @@ export default class DriverDetails extends React.Component {
                     <Text style={styles.urlStyle} onPress={() => Linking.openURL(item.url)}>{`Books: ${item.url}`}</Text>
                 </View>
                 <View>
-                    <Image source={this.state.driverThumbImage} />
+                    <Image source={driverThumbIndex[item.driverId]} />
                 </View>
             </ImageBackground>
         )
